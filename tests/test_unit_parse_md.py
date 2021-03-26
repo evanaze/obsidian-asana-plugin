40bf53c6a5a0955843bb63b892114ba6e57b0f42
Evan Azevedo <evan@azevedo.com>
2021-03-26 06:34:42 -0700

    try to run unit test

--- tests/test_unit_parse_md.py
+++ tests/test_unit_parse_md.py
@@ -1,4 +1,13 @@
+import os
 import pytest
 
-def test_parse_task():
-    
+class ParseTests:
+    def __init__(self):
+        # the base directory for test markdown files
+        self.basedir = os.path.join(os.getcwd(), "tasks/") 
+
+    def test_parse_task(self):
+        filename = os.path.join(self.basedir, "sampleTask.md")
+        print(filename)
+        print(parse_task.parse_md(filename))
+        assert 0
