8603ac0667b25e2f4ea4a97ca08db6285c91e000
Evan Azevedo <evan@azevedo.com>
2021-03-26 07:14:15 -0700

    first attempt at parsing md

--- obsidian_asana_plugin/parse_task.py
+++ obsidian_asana_plugin/parse_task.py
@@ -6,11 +6,32 @@
 def parse_md(filename):
     "Parses a markdown file in the Task.md format"
     # initialize the task object
-    task = {}
+    task = {"description": ''}
     # start reading the input file
     with open(filename, "r") as read_obj:
+        count = 0
         while True:
-            line = read_obj.readline
+            line = read_obj.readline()
             if not line:
                 break
-            print(line)
+            if count == 2:
+                task['title'] = ' '.join(line.split()[1:])
+            if count == 3:
+                task['status'] = line.split()[1]
+            if count == 4:
+                task['priority'] = line.split()[1]
+            if count == 5:
+                task['due_date'] = line.split()[2]
+            if count == 6:
+                task['start_date'] = line.split()[2]
+            if count == 7:
+                task['deliverable'] = ' '.join(line.split()[1:])
+            if count >= 9:
+                task['description'] += line
+            count += 1
+    print(task)
+    return task
+
+
+if __name__ == "__main__":
+    parse_md("tests/tasks/sampleTask.md")
--- tests/test_unit_parse_md.py
+++ tests/test_unit_parse_md.py
@@ -2,13 +2,7 @@ import os
 import pytest
 from obsidian_asana_plugin.parse_task import parse_md
 
-class ParseTests:
-    def __init__(self):
-        # the base directory for test markdown files
-        self.basedir = os.path.join(os.getcwd(), "tasks/") 
-
-    def test_parse_task(self):
-        filename = os.path.join(self.basedir, "sampleTask.md")
-        print(filename)
-        print(parse_md(filename))
-        assert 0
+def test_parse_task():
+    filename = os.path.join(os.getcwd(), "tests/tasks/sampleTask.md")
+    result = parse_md(filename)
+    assert result == {}
