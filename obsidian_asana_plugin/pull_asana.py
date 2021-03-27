13e7db0255339a8d90d02f24088a97eda6e510a0
Evan Azevedo <evan@azevedo.com>
2021-03-27 12:21:25 -0700

    connecting to api

--- obsidian_asana_plugin/pull_asana.py
+++ obsidian_asana_plugin/pull_asana.py
@@ -1,41 +1,30 @@
 """ Pulls data from Asana using the API """
 import os
-import json
-import requests
 import asana
 from dotenv import load_dotenv
 from log import logger
 
-def api_call(url):
-    "Makes a call to the Asana API given a url"
-    headers = {'Authorization': f"Bearer {api_key}"}
-    response = requests.get(url, headers=headers)
-    try:
-        return json.loads(response.text)
-    except Exception:
-        raise Exception
 
-def test_connection():
+def connect():
     "Connects to the Asana API"
     # get the api key from .env file
     load_dotenv()
     api_key = os.getenv("ASANA_API_KEY")
     # connect to the api
-    url = "https://app.asana.com/api/1.0/users/me"
-    response = api_call(url)
-    if response["data"] is not None:
-        logger.info("API Connection successful")
+    client = asana.Client.access_token(api_key)
+    return client
 
-def projects():
+def projects(client):
     "Gets a list of projects"
-    api_call()
+    return client.projects.get_projects(opt_pretty=True)
 
 def main():
     "Executes the main workflow"
     # test the connection to the API
-    test_connection()
+    client = connect()
+    print(client)
     # get a list of projects
-
+    print(projects(client))
 
 if __name__ == "__main__":
-    connect()
+    main()
